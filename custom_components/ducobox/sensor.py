9478a887b8f2f32a1cff68ef726ef1cfaa8b064b
Eli Hamels <eli.hamels@kuleuven.be>
2023-03-29 10:29:02 +0000

    implement DataUpdateCoordinator

--- custom_components/ducobox/sensor.py
+++ custom_components/ducobox/sensor.py
@@ -17,11 +17,19 @@ from homeassistant.helpers.entity import DeviceInfo, Entity
 from homeassistant.config_entries import ConfigEntry
 
 
+from homeassistant.helpers.update_coordinator import (
+    CoordinatorEntity,
+    DataUpdateCoordinator,
+    UpdateFailed,
+)
+
 from . import DOMAIN
 from .ducobox import GenericSensor, DucoBox
 from datetime import timedelta
 
 SCAN_INTERVAL = timedelta(seconds=5)
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(
@@ -34,32 +42,67 @@ async def async_setup_entry(
     # __init__.async_setup_entry function
     dbb = hass.data[DOMAIN][config_entry.entry_id]
 
+    coordinator = MyCoordinator(hass, dbb)
+
+    # Fetch initial data so we have data when entities subscribe
+    #
+    # If the refresh fails, async_config_entry_first_refresh will
+    # raise ConfigEntryNotReady and setup will try again later
+    #
+    # If you do not want to retry setup on failure, use
+    # coordinator.async_refresh() instead
+    #
+    await coordinator.async_config_entry_first_refresh()
+
     # master_module = dbb.modules[0]
     for module in dbb.modules:
         device_id = module.base_adr
-        sensors = [DocuSensor(sensor, device_id) for sensor in module.sensors]
-        sensors += [DocuSensor(sensor, device_id) for sensor in module.actuators]
-
-        for sens in sensors:
-            print(sens.unique_id)
+        sensors = [
+            DocuSensor(coordinator, sensor, device_id) for sensor in module.sensors
+        ]
+        sensors += [
+            DocuSensor(coordinator, sensor, device_id) for sensor in module.actuators
+        ]
 
         # Add all entities to HA
         async_add_entities(sensors, update_before_add=True)
 
-    # async_add_entities(
-    #    [DocuSensor(sensor, config_entry.entry_id) for sensor in dbb.actuators],
-    #    update_before_add=True,
-    # )
+
+class MyCoordinator(DataUpdateCoordinator):
+    """My custom coordinator."""
+
+    def __init__(self, hass, dbb):
+        """Initialize my coordinator."""
+        super().__init__(
+            hass,
+            _LOGGER,
+            # Name of the data. For logging purposes.
+            name="DucoBox coordinator",
+            # Polling interval. Will only be polled if there are subscribers.
+            update_interval=timedelta(seconds=30),
+        )
+        self.dbb = dbb
+
+    async def _async_update_data(self):
+        """Fetch data from API endpoint.
+
+        This is the place to pre-process the data to lookup tables
+        so entities can quickly look up their data.
+        """
+        await self.dbb.update_sensors()
 
 
-class DocuSensor(SensorEntity):
+class DocuSensor(CoordinatorEntity, SensorEntity):
     """Representation of a sensor."""
 
     _attr_should_poll = True
     _attr_unique_id = True
 
-    def __init__(self, sens: GenericSensor, device_id) -> None:
+    def __init__(
+        self, coordinator: MyCoordinator, sens: GenericSensor, device_id
+    ) -> None:
         """Initialize the sensor."""
+        super().__init__(coordinator, context=device_id)
         self.sens_obj = sens
         self.device_id = device_id
 
@@ -93,12 +136,6 @@ class DocuSensor(SensorEntity):
     def unit_of_measurement(self) -> str:
         """Return the unit of measurement."""
         return self.unit
-
-    async def async_update(self) -> None:
-        """Fetch new state data for the sensor.
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        await self.sens_obj.update()
 
     @property
     def device_info(self) -> DeviceInfo:
